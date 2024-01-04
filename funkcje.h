4523310008e92a738cff551d091d93a594acfd32
Fifi211 <131053662+Fifi211@users.noreply.github.com>
2024-01-05 00:06:03 +0100

    Add files via upload

---
+++ funkcje.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class CalculatorFunctions {
+public:
+    double naturalLogarithm(double x);
+    double standardDeviation(double x1, double x2, double x3, double x4, double x5);
+    double exponentValue(double x);
+
+private:
+    double calculateMean(double x1, double x2, double x3, double x4, double x5);
+};
